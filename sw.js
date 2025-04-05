c638d8baaf3765ab9499519f9e80260c274980af
joseluisDCO <decastro.olmedo@gmail.com>
2025-04-05 20:44:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+
+const CACHE_NAME = "sap-sen-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js",
+  "/icon-sap-192x192.png",
+  "/icon-sap-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
